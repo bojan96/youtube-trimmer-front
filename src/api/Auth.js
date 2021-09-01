f19e244abee09af80c8b76972c017f02e68e8b10
Bojan Malinic <bojan.malinic@outlook.com>
2021-09-01 12:14:44 +0200

    Store token on successful login

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -2,13 +2,14 @@ import { bearerAuthHeaders } from "../util/AuthUtils";
 import { makeEndpoint } from "../util/UrlUtils";
 
 const API_URL = "http://localhost:8080"
+const TOKEN_ITEM = "token"
 
 function createRequestUrl(endpoint) {
     return makeEndpoint(API_URL, endpoint);
 }
 
 export async function isUserLoggedIn() {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem(TOKEN_ITEM);
 
     if (token == null) {
         return false;
@@ -31,9 +32,13 @@ export async function login(username, password) {
 
     const response = await fetch(url, {
         method: "POST",
-        body: JSON.stringify({username, password}),
-        headers: new Headers({'content-type': 'application/json'})
+        body: JSON.stringify({ username, password }),
+        headers: new Headers({ 'content-type': 'application/json' })
     });
+
+    if (response.ok) {
+        localStorage.setItem(TOKEN_ITEM, await response.text())
+    }
 
     return response.ok;
 }
