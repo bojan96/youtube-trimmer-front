1342eb6079e960d57ab2e429e8ba916a7bf1dda2
Bojan Malinic <bojan.malinic@outlook.com>
2021-09-02 22:07:59 +0200

    Implement initial version of Job component

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -1,18 +1,66 @@
-import { Button, Container, TextField } from "@material-ui/core";
+import { Container, TextField } from "@material-ui/core";
+import { useState } from 'react';
+import LoadingButton from '../LoadingButton/LoadingButton';
+import * as JobApi from '../../api/Job';
 import styles from './Job.module.css'
+import { useHistory } from "react-router-dom";
 
 
 export default function Job(props) {
 
+    const [videoUrl, setVideoUrl] = useState("");
+    const [trimFrom, setTrimFrom] = useState(0);
+    const [trimTo, setTrimTo] = useState(0);
+    const [isBusy, setIsBusy] = useState(false);
+    const history = useHistory();
+
     return (
         <Container maxWidth="xs" {...props}>
-            <form>
-                <TextField required fullWidth label="Youtube URL"/>
-                <Container disableGutters={true} className={styles.time_container}>
-                    <TextField label="From" required type="number"/>
-                    <TextField label="To" required type="number"/>
+            <form onSubmit={ev => {
+                ev.preventDefault();
+                setIsBusy(true);
+                JobApi.createJob({ trimFrom, trimTo, videoUrl })
+                    .then(() => {
+                        setIsBusy(false);
+                        history.push("/job");
+                    });
+
+            }}>
+                <TextField
+                    value={videoUrl}
+                    onChange={ev => setVideoUrl(ev.target.value)}
+                    required
+                    fullWidth
+                    label="Youtube URL" />
+                <Container
+                    disableGutters={true}
+                    className={styles.time_container}>
+                    <TextField
+                        inputProps={{ min: 0 }}
+                        value={trimFrom}
+                        onChange={ev => setTrimFrom(ev.target.value)}
+                        label="From"
+                        required
+                        type="number" />
+                    <TextField
+                        inputProps={{ min: 0 }}
+                        value={trimTo}
+                        onChange={ev => setTrimTo(ev.target.value)}
+                        label="To"
+                        required
+                        type="number" />
                 </Container>
-                <Button className={styles.submit_button} color="primary" variant="contained" type="submit" fullWidth>Submit</Button>
+                <LoadingButton
+                    spinnerProps={{
+                        size: 25,
+                        className: "loading_button_spinner"
+                    }}
+                    isBusy={isBusy}
+                    className={styles.submit_button}
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    fullWidth>Submit</LoadingButton>
             </form>
         </Container>
     );
