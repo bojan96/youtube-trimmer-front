d857fb1ae741c3b225e16a46f7c39078bf674224
Bojan Malinic <bojan.malinic@outlook.com>
2021-09-02 17:37:53 +0200

    Implement login form submission using form submit event

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,23 @@
 import styles from './Login.module.css';
 import { useState } from 'react';
-import { Container, TextField} from '@material-ui/core';
+import { Container, TextField } from '@material-ui/core';
 import LoadingButton from '../LoadingButton/LoadingButton';
 
-function Login({onLoginClick = () => {}, isBusy, className}) {
+function Login({ onLoginClick = () => { }, isBusy, className }) {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     return (
         <Container className={className} maxWidth="xs">
-            <form>
+            <form onSubmit={ev => {
+                ev.preventDefault();
+                onLoginClick(username, password);
+            }}>
                 <TextField label="Username" value={username} onChange={ev => setUsername(ev.target.value)} required fullWidth />
                 <TextField label="Password" value={password} onChange={ev => setPassword(ev.target.value)} required fullWidth />
-                <LoadingButton disabled={isBusy} isBusy={isBusy} onClick={() => onLoginClick(username, password)} variant="contained" color="primary" 
-                fullWidth className={styles.login_button} spinnerProps={{size: 25, className: styles.spinner}}>
+                <LoadingButton type="submit" disabled={isBusy} isBusy={isBusy} variant="contained" color="primary"
+                    fullWidth className={styles.login_button} spinnerProps={{ size: 25, className: styles.spinner }}>
                     Login
                 </LoadingButton>
             </form>
