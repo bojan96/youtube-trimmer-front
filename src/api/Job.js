26a77664a77bd3d9213327afcf61b4e77e70eb37
Bojan Malinic <bojan.malinic@outlook.com>
2021-09-03 17:29:15 +0200

    Implement cancel job api call

--- src/api/Job.js
+++ src/api/Job.js
@@ -10,31 +10,42 @@ export async function createJob(job) {
     const response = await fetch(endpoint, {
         method: "POST",
         body: JSON.stringify(job),
-        headers: bearerAuthHeaders(token, {"content-type": "application/json"})
+        headers: bearerAuthHeaders(token, { "content-type": "application/json" })
     });
-    
-    if(!response.ok)
-    {
+
+    if (!response.ok) {
         throw new Error(`Unable to create a job, status code: ${response.status}`);
     }
 }
 
-export async function getJobs()
-{
+export async function getJobs() {
     const endpoint = makeEndpoint(config.API_URL, "/api/job")
     const token = localStorage.getItem(config.TOKEN_ITEM);
 
     const response = await fetch(endpoint, {
         method: "GET",
-        headers: bearerAuthHeaders(token, {"content-type": "application/json"})
+        headers: bearerAuthHeaders(token, { "content-type": "application/json" })
     });
 
-    if(!response.ok)
-    {
+    if (!response.ok) {
         throw new Error(`Unable to retrieve user jobs, status code: ${response.status}`);
     }
 
-    return response.json();
+    return await response.json();
+}
+
+export async function cancelJob(jobId) {
+    const endpoint = makeEndpoint(config.API_URL, `/api/job/${jobId}/cancel`)
+    const token = localStorage.getItem(config.TOKEN_ITEM);
+
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: bearerAuthHeaders(token, { "content-type": "application/json" })
+    });
+
+    if (!response.ok) {
+        throw new Error(`Unable to cancel job, status code: ${response.status}`);
+    }
 }
 
 
