b95865bf70092c093ce554f2a3d95644e8c00958
Bojan Malinic <bojan.malinic@outlook.com>
2021-09-05 14:27:16 +0200

    Replace confirmation button with LoadingButton

--- src/components/ConfirmDialog/ConfirmDialog.js
+++ src/components/ConfirmDialog/ConfirmDialog.js
@@ -1,11 +1,11 @@
 import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core";
+import LoadingButton from "../LoadingButton/LoadingButton";
 
 
 export default function ConfirmDialog({ title, text, confirmBttnText, rejectBttnText, onClose, isBusy, ...rest }) {
     return (
         <Dialog onClose={(data, reason) => {
-            if(isBusy)
-            {
+            if (isBusy) {
                 return;
             }
             onClose(data, reason);
@@ -16,11 +16,20 @@ export default function ConfirmDialog({ title, text, confirmBttnText, rejectBttn
             </DialogContent>
             <DialogActions>
                 <Button disabled={isBusy} onClick={() => onClose(null, "reject")} variant="contained" color="secondary">{rejectBttnText}</Button>
-                <Button disabled={isBusy} onClick={() => onClose(null, "confirm")} variant="contained" color="primary">
+
+                <LoadingButton
+                    disabled={isBusy}
+                    onClick={() => onClose(null, "confirm")}
+                    variant="contained"
+                    color="primary"
+                    spinnerProps={{
+                        size: 25,
+                        className: "loading_button_spinner"
+                    }}>
                     {
                         confirmBttnText
                     }
-                    {isBusy ? <CircularProgress size={20} className="loading_button_spinner"/> : null} </Button>
+                </LoadingButton>
             </DialogActions>
         </Dialog>);
 }
