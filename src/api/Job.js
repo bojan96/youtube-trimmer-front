2e3968a63b7957f0b139844f24905b0c8e1edd0c
Bojan Malinic <bojan.malinic@outlook.com>
2021-09-07 02:44:18 +0200

    Remove error handling code

--- src/api/Job.js
+++ src/api/Job.js
@@ -8,15 +8,11 @@ export async function createJob(job) {
     const endpoint = makeEndpoint(config.API_URL, "/api/job");
     const token = localStorage.getItem(config.TOKEN_ITEM)
 
-    const response = await fetch(endpoint, {
+    await fetch(endpoint, {
         method: "POST",
         body: JSON.stringify(job),
         headers: bearerAuthHeaders(token, { "content-type": "application/json" })
     });
-
-    if (!response.ok) {
-        throw new Error(`Unable to create a job, status code: ${response.status}`);
-    }
 }
 
 export async function getJobs() {
@@ -28,10 +24,6 @@ export async function getJobs() {
         headers: bearerAuthHeaders(token, { "content-type": "application/json" })
     });
 
-    if (!response.ok) {
-        throw new Error(`Unable to retrieve user jobs, status code: ${response.status}`);
-    }
-
     return await response.json();
 }
 
@@ -39,14 +31,10 @@ export async function cancelJob(jobId) {
     const endpoint = makeEndpoint(config.API_URL, `/api/job/${jobId}/cancel`)
     const token = localStorage.getItem(config.TOKEN_ITEM);
 
-    const response = await fetch(endpoint, {
+    await fetch(endpoint, {
         method: "POST",
         headers: bearerAuthHeaders(token, { "content-type": "application/json" })
     });
-
-    if (!response.ok) {
-        throw new Error(`Unable to cancel job, status code: ${response.status}`);
-    }
 }
 
 function unsubscribe()
